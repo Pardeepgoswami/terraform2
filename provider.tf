0bda954e1be6aa53e5fb05b6c007c558eea72528
Pardeepgoswami <purid797@gmail.com>
2023-12-15 15:13:11 +0530

    s3-bucket

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 0.14.11"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  backend "s3" {
+    bucket = "kdnekoen"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+provider "aws" {
+  region = "us-east-1"  # Change this to your desired region
+}
